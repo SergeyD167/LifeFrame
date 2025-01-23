7c5505a279132f732151255ce3b1d8620a58d6ef
Sergey Dyatlov <sergeyd020501@gmail.com>
2025-01-23 21:19:18 +0300

    Initial Commit

---
+++ LifeFrame/LifeFrameApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeFrameApp.swift
+//  LifeFrame
+//
+//  Created by Сергей Дятлов on 23.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct LifeFrameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
